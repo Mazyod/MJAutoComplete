34593823e46ff94bd1b3488c6d273e93887b010e
Mazyad Alabduljaleel <mazjaleel@gmail.com>
2014-02-12 18:14:11 +0400

    = This is probably the best way to handle delimiters.

--- MJAutoComplete/MJAutoCompleteManager.m
+++ MJAutoComplete/MJAutoCompleteManager.m
@@ -48,10 +48,10 @@
             NSRange dlRange = [weakSelf.processingString rangeOfString:delimiter options:NSBackwardsSearch];
             if (!delimiter.length)
             {
-                dlRange = NSMakeRange(0, weakSelf.processingString.length);
+                dlRange = NSMakeRange(0, 0);
             }
 
-            NSString* prevString = [weakSelf.processingString substringToIndex:dlRange.location+delimiter.length];
+            NSString* prevString = [weakSelf.processingString substringToIndex:NSMaxRange(dlRange)-delimiter.length];
             NSString* newString = [NSString stringWithFormat:@"%@%@ ", prevString, autoCompleteString];
             
             [weakSelf.delegate autoCompleteManager:weakSelf shouldUpdateToText:newString];
